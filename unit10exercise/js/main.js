7a29ff69fc4f902e1c07b6fab86bff3c1f4a7d47
Tim Peters <Tim@Tim-Peterss-MacBook-Pro.local>
2016-03-07 15:54:54 -0600

    Unit 10 exercise update

--- unit10exercise/js/main.js
+++ unit10exercise/js/main.js
@@ -1,32 +1,34 @@
 // https://learn.jquery.com/using-jquery-core/document-ready/
 
 
+$(document).ready(function(){
 
+    $("#movie-menu").click(function(anchors){
 
-    $( "a[data-target*='episode']" ).click(function(){
+            //Hide any movie descriptions already showing
+                $(".movies.row").hide( "slow" );
 
-        //Hide any movie descriptions already showing
-            $(".movies.row").hide( "slow" );
+            //Color the clicked <a> yellow
+                $(anchors.target).css("color", "yellow");
 
-        //Color the clicked <a> yellow
-            $(this).css("color", "yellow");
+            //Color the clicked <a>'s parent <ul>'s backgroud black
+                $(anchors.target).parent().css("backgroundColor", "black");
 
-        //Color the clicked <a>'s parent <ul>'s backgroud black
-            $(this).parent().css("backgroundColor", "black");
+            //Clear the (background) styling from other <ul>'s
+                $(anchors.target).parent().siblings().removeAttr( "style" ); 
 
-        //Clear the (background) styling from other <ul>'s
-            $(this).parent().siblings().removeAttr( "style" ); 
+            //Clear the (text color) styling from other <a>'s
+                $(anchors.target).parent().siblings().children().removeAttr( "style" );
+                
+            //Color the clicked <a>'s corresponding <div>'s color to yellow & bg color to black
+                $("#" + $(anchors.target).data("target")).css({"color": "yellow", "backgroundColor": "black"});
 
-        //Clear the (text color) styling from other <a>'s
-            $(this).parent().siblings().children().removeAttr( "style" );
-            
-        //Color the clicked <a>'s corresponding <div>'s color to yellow & bg color to black
-            $("#" + $(this).attr("data-target")).css({"color": "yellow", "backgroundColor": "black"});
-
-        //Show the clicked <a>'s corresponding <div>
-            $("#" + $(this).attr("data-target")).show( "slow" );
+            //Show the clicked <a>'s corresponding <div>
+                $("#" + $(anchors.target).data("target")).show( "slow" );
    
      });
+
+});
 
 
 
--- unit9assignment/js/main.js
+++ unit9assignment/js/main.js
@@ -1,30 +1,34 @@
 // https://learn.jquery.com/using-jquery-core/document-ready/
 
 
+<<<<<<< Updated upstream
 
 
     $( "a[data-target*='episode']" ).click(function(){
+=======
+    $("#movie-menu").click(function(anchors){
+>>>>>>> Stashed changes
 
         //Hide any movie descriptions already showing
             $(".movies.row").hide( "slow" );
 
         //Color the clicked <a> yellow
-            $(this).css("color", "yellow");
+            $(anchors.target).css("color", "yellow");
 
         //Color the clicked <a>'s parent <ul>'s backgroud black
-            $(this).parent().css("backgroundColor", "black");
+            $(anchors.target).parent().css("backgroundColor", "black");
 
         //Clear the (background) styling from other <ul>'s
-            $(this).parent().siblings().removeAttr( "style" ); 
+            $(anchors.target).parent().siblings().removeAttr( "style" ); 
 
         //Clear the (text color) styling from other <a>'s
-            $(this).parent().siblings().children().removeAttr( "style" );
+            $(anchors.target).parent().siblings().children().removeAttr( "style" );
             
         //Color the clicked <a>'s corresponding <div>'s color to yellow & bg color to black
-            $("#" + $(this).attr("data-target")).css({"color": "yellow", "backgroundColor": "black"});
+            $("#" + $(anchors.target).data("target")).css({"color": "yellow", "backgroundColor": "black"});
 
         //Show the clicked <a>'s corresponding <div>
-            $("#" + $(this).attr("data-target")).show( "slow" );
+            $("#" + $(anchors.target).data("target")).show( "slow" );
    
      });
 
