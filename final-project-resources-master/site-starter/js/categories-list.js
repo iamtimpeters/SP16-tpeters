5d223644368fe39ac0a58a4036e62e51788a396c
Tim Peters <Tim@Tim-Peterss-MacBook-Pro.local>
2016-04-16 17:25:41 -0500

    final - changes

--- final-project-resources-master/site-starter/js/categories-list.js
+++ final-project-resources-master/site-starter/js/categories-list.js
@@ -1,1 +1,25 @@
 // custom code for parkland bookstore
+
+$(document).ready(function(){
+    
+    var url = "https://parkland-csc175.github.io/csc175data/bestbuy/categories-list.json";
+
+    $.get(url, /* callback */ function(result){
+
+    	var categoriesData = result.categories;
+
+    	console.log(categoriesData)
+
+    	function AddingCategories(){
+
+    		var self = this;
+
+    		self.categories = ko.observableArray(categoriesData);
+
+    	};
+
+    ko.applyBindings(new AddingCategories());
+
+    });
+
+});
