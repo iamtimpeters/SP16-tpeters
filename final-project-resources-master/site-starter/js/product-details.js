7ad44d6c3fd6e39043a92e945d50f6aca6541949
Tim Peters <Tim@Tim-Peterss-MacBook-Pro.local>
2016-04-17 15:25:54 -0500

    final - testing

--- final-project-resources-master/site-starter/js/product-details.js
+++ final-project-resources-master/site-starter/js/product-details.js
@@ -1,1 +1,25 @@
 // custom code for parkland bookstore
+
+$(document).ready(function(){
+    
+    var url = "https://parkland-csc175.github.io/csc175data/bestbuy/product-details-4506800.json";
+
+    $.get(url, /* callback */ function(result){
+
+    	var productsDetailData = result.products;
+
+    	console.log(productsDetailData);
+
+    	function AddingProductDetail(){
+
+    		var self = this;
+
+    		self.details = ko.observableArray(productsDetailData);
+
+    	};
+
+    ko.applyBindings(new AddingProductDetail());
+
+    });
+
+});
--- final-project-resources-master/site-starter/js/products-list.js
+++ final-project-resources-master/site-starter/js/products-list.js
@@ -1,1 +1,25 @@
 // custom code for parkland bookstore
+
+$(document).ready(function(){
+    
+    var url = "https://parkland-csc175.github.io/csc175data/bestbuy/products-list.json";
+
+    $.get(url, /* callback */ function(result){
+
+    	var productsData = result.products;
+
+    	console.log(productsData);
+
+    	function AddingProducts(){
+
+    		var self = this;
+
+    		self.products = ko.observableArray(productsData);
+
+    	};
+
+    ko.applyBindings(new AddingProducts());
+
+    });
+
+});
