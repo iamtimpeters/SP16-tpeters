f4253245dc00d3ef62fc5b5f1acb35cc66be807f
Tim Peters <Tim@Tim-Peterss-MacBook-Pro.local>
2016-04-21 23:18:50 -0500

    final - still more

--- final-project-resources-master/site-starter/js/product-details.js
+++ final-project-resources-master/site-starter/js/product-details.js
@@ -2,7 +2,15 @@
 
 $(document).ready(function(){
     
-    var url = "https://parkland-csc175.github.io/csc175data/bestbuy/product-details-4506800.json";
+    //var url = "https://parkland-csc175.github.io/csc175data/bestbuy/product-details-4506800.json";
+
+    var urlFirstHalf = "https://api.bestbuy.com/v1/products(sku="
+    
+    var productCategory = document.location.hash.slice(1)
+    
+    var urlSecondHalf = ")?format=json&apiKey=zap8x2yv8ryc6jc9bet42dkp"
+    
+    var url = urlFirstHalf + productCategory + urlSecondHalf;
 
     $.get(url, /* callback */ function(result){
 
--- final-project-resources-master/site-starter/js/products-list.js
+++ final-project-resources-master/site-starter/js/products-list.js
@@ -1,20 +1,35 @@
 // custom code for parkland bookstore
 
 $(document).ready(function(){
+
+    var urlFirstPart = "https://api.bestbuy.com/v1/products(categoryPath.id="
     
-    var url = "https://parkland-csc175.github.io/csc175data/bestbuy/products-list.json";
+    var productCategory = document.location.hash.slice(1)
+    
+    var urlSecondPart = ")?apiKey=zap8x2yv8ryc6jc9bet42dkp&sort=name.asc&page="
+
+    var productPageNum = 1
+
+    var urlThirdPart = "&format=json"
+    
+    var url = urlFirstPart + productCategory + urlSecondPart + productPageNum + urlThirdPart;
 
     $.get(url, /* callback */ function(result){
 
     	var productsData = result.products;
+        var currentPage = result.currentPage;
 
     	console.log(productsData);
+
+        console.log(currentPage);
 
     	function AddingProducts(){
 
     		var self = this;
 
     		self.products = ko.observableArray(productsData);
+
+            //self.thisPage = ko.observableArray(currentPage);
 
             self.productDetailLink = function(products){
                 var localSource = "file:///Users/Tim/Documents/Parkland%20classes/CSC175/SP16-tpeters/final-project-resources-master/site-starter/product-details.html";    
@@ -23,6 +38,24 @@ $(document).ready(function(){
                 console.log(document.location.hash);
                 console.log(document.location.hash.slice(1));
                 document.location.assign(localSource);
+            };
+
+            self.nextPage = function(){
+                
+               var nextPage = productPageNum + 1
+
+               url = urlFirstPart + productCategory + urlSecondPart + nextPage + urlThirdPart;
+
+               $.get(url, /* callback */ function(newResult){
+
+                    self.products.push(newResult.products);
+
+                    //var newProductsData = newResult.products;
+
+                    //self.products = ko.observableArray(newProductsData);
+
+                });
+
             };
 
     	};
